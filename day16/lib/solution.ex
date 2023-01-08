7ac3d603ec61e55e54c3c6b66348163aaa2245ec
Boris Okner <boris.okner@gmail.com>
2023-01-08 16:20:43 -0500

    Split valves with rate > 0 into two nodes

--- day16/lib/solution.ex
+++ day16/lib/solution.ex
@@ -49,24 +49,24 @@ defmodule Day16.Solution do
   end
 
   defp solve_mzn({valves, connections, rates} = _data, part) do
-    {model, minutes} =
+    {model, minutes, solver} =
       case part do
-        :part1 -> {"model/valves.mzn", 30}
-        :part2 -> {"model/valves_part2.mzn", 26}
+        :part1 -> {"model/valves.mzn", 30, "or_tools"}
+        :part1_ext -> {"model/valves-ext.mzn", 30, "chuffed"}
+        :part2 -> {"model/valves_part2.mzn", 26, "or-tools"}
       end
 
-    dzn = %{
-      minutes: minutes,
-      valves: List.to_tuple(valves),
-      connections: {["valves"], connections},
-      rates: {["valves"], rates}
-    }
+
+    dzn = build_dzn(valves, connections, rates)
+    |> Map.put(:minutes, minutes)
+
+
 
     {:ok, solution} =
       MinizincSolver.solve_sync(
         model,
         dzn,
-        solver: "or-tools",
+        solver: solver,
         time_limit: 60 * 60 * 1000
       )
 
@@ -76,4 +76,27 @@ defmodule Day16.Solution do
       Logger.info("Solution: #{result}")
     end)
   end
+
+  defp build_dzn(valves, connections, rates) do
+    {updated_rates, updated_valves, updated_connections} = List.foldr(Enum.zip([rates, valves, connections]), {[], [], []},
+
+    fn {0, valve, conns}, {rates_acc, valves_acc, conns_acc} -> {[0 | rates_acc], [valve | valves_acc], [conns | conns_acc]}
+      {rate, valve, conns}, {rates_acc, valves_acc, conns_acc} ->
+        new_rates = [0, rate | rates_acc]
+        dummy_valve = valve<>"_closed"
+        new_valves = [valve, dummy_valve | valves_acc]
+        updated_connection = MapSet.put(conns, dummy_valve)
+        new_connections = [updated_connection, conns |conns_acc]
+        {new_rates, new_valves, new_connections}
+    end)
+
+    IO.inspect(Enum.zip(updated_rates, updated_valves) |> Enum.filter(fn {r, _} -> r == 0 end))
+
+    %{
+      valves: List.to_tuple(updated_valves),
+      connections: {["valves"], updated_connections},
+      rates: {["valves"], updated_rates}
+    }
+  end
+
 end
